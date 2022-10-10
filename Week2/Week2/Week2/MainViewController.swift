1914db7e9b045ee5086e54dc9642504427ff6a72
SoHyeon Kim <happythgus89@gmail.com>
2022-10-10 17:05:17 +0900

    clone coding

---
+++ Week2/Week2/Week2/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Week2
+//
+//  Created by 김소현 on 2022/09/27.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ Week2/Week2/Week2/ModalViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ModalViewController.swift
+//  Week2
+//
+//  Created by 김소현 on 2022/09/28.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
