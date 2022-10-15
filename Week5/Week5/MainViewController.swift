d87a02234d52e483fc4c13b2d6391c8f3d7d7f4f
SoHyeon Kim <happythgus89@gmail.com>
2022-10-15 17:43:22 +0900

    UI-Kit

---
+++ Week5/Week5/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  Week5
+//
+//  Created by 김소현 on 2022/10/15.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
