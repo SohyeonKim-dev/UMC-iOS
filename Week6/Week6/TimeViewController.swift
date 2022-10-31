91c701ab895ac0e3c3983a89fd560c9fc2257346
SoHyeon Kim <happythgus89@gmail.com>
2022-10-31 22:29:19 +0900

    Add files via upload

---
+++ Week6/Week6/TimeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TimeViewController.swift
+//  Week6
+//
+//  Created by 김소현 on 2022/10/31.
+//
+
+import UIKit
+
+class TimeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
